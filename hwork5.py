a620a7aae33f4626af68ee6d4044654a94e2867e
Alexey <112006201+Lexan4ik1991@users.noreply.github.com>
2023-11-01 10:52:25 +0400

    Update hwork5.py

--- hwork5.py
+++ hwork5.py
@@ -4,4 +4,4 @@ sum([Head|Tail], Sum) :-
     sum(Tail, TempSum),
     Sum is Head + TempSum.
 
-?- sum([1, 2, 3, 4, 5,6,7,8,9,10], Sum), write(Sum).
+?- sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], Sum), write(Sum).
